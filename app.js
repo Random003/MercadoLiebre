b5556e0eb586ca041b4a89e62c6f84bf8d13b1a5
Random003 <mealla.jp@gmail.com>
2020-07-03 00:03:36 -0300

    arreglos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-// para que tome la carpeta public
+// para que tome la carpeta public, no se necesita poner la ruta completa sino siguiente de public
 app.use(express.static("public"));
 
 // para que la pagina no tire el cant get /
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
+// para abrir el listen en el puerto determinado
 app.listen(4001, () => { console.log("Código funcionando y escuchando en el puerto 4001; coloca -localhost/4001- en el navegador por favor.")})
