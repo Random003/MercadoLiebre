eeba2cd6f90ba03c2118892577c255fb3915383d
Random003 <mealla.jp@gmail.com>
2020-07-07 23:36:43 -0300

    agregado pagina de registro y login

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 
 # **Ejercitación e-comerce de Clases Sincrónicas de Curso "Full Stack" en Digital House**
 
-## Contedio de las clases:
+## Contenido tratado en las clases:
 
 Fecha | Descripción | Herramienta
 ----- | ----------- | -----------
 30/6 | Estructura base en mobile de aplicación. | HTML, Express.
 2/7 | Agregar CSS en vista mobile. | CSS.
-7/7 |   |
+7/7 | Formularios. | HTML.
+14/7 |  |
--- app.js
+++ app.js
@@ -5,10 +5,34 @@ const app = express();
 // para que tome la carpeta public, no se necesita poner la ruta completa sino siguiente de public
 app.use(express.static("public"));
 
+// esto es para que ande el app.post y al mandar el formulario no lo envia nada asiqu PONER
+app.use(express.urlencoded({extended: false}))
+
 // para que la pagina no tire el cant get /
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+// para que ande el post 
+app.post("/", (req, res) => {
+    res.send(req.body);
+});
+
+app.post("/register", (req, res) => {
+    res.send(req.body);
+});
+
+app.post("/login", (req, res) => {
+    res.send(req.body);
+});
+
 // para abrir el listen en el puerto determinado
-app.listen(4001, () => { console.log("Código funcionando y escuchando en el puerto 4001; coloca -localhost/4001- en el navegador por favor.")})
+app.listen(4001, () => { console.log("Código funcionando y escuchando en el puerto 4001;  igual recorda de colocar -npm run startcode- y despues coloca -localhost/4001- en el navegador por favor.")})
